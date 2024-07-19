72f6d94d6248f6204f0b3ce9753c9005896b04f7
hlysllrs <hailey.sellars@gmail.com>
2024-07-19 17:07:07 -0400

    slideshow intro fade in

--- scripts/slides.js
+++ scripts/slides.js
@@ -11,6 +11,25 @@ slides.forEach((slide) => {
     image.style.zIndex = z
   })
 
+  const timeline = gsap.timeline()
+
+  timeline
+    .set(images, {
+      x: () => {
+        return 500 * Math.random() - 250
+      },
+      y: '500%',
+      rotation: () => {
+        return 90 * Math.random() - 45
+      },
+    })
+    .to(images, { x: 0, y: 0, stagger: -0.25 })
+    .to(images, {
+      rotation: () => {
+        return 12 * Math.random() - 6
+      },
+    })
+
   slide.addEventListener('click', (e) => {
     z--
     images[current].style.zIndex = z
