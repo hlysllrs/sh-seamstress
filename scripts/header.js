a8553eadebd48b1ee9b4fb6b7d27764b8e0216f0
hlysllrs <hailey.sellars@gmail.com>
2024-07-19 16:31:48 -0400

    header fade in animation

---
+++ scripts/header.js
@@ -0,0 +1,7 @@
+const carousels = document.querySelectorAll('header h1, header h2')
+
+const fadeInTimeline = gsap.timeline()
+
+fadeInTimeline
+  .set(carousels, { opacity: 0 })
+  .to(carousels, { opacity: 1, delay: 1, stagger: 1, duration: 3 })
