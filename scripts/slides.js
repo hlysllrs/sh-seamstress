6609b7b370dd41b9575f28abe143fa3f18efa681
hlysllrs <hailey.sellars@gmail.com>
2024-07-19 16:13:54 -0400

    reverse slideshow direction

--- scripts/slides.js
+++ scripts/slides.js
@@ -1,12 +1,17 @@
 const slides = document.querySelector('section div.slides')
 
 let current = 0
-let z = 0
+let z = 100000000
 
 const images = slides.querySelectorAll('img')
 
+images.forEach((image) => {
+  z--
+  image.style.zIndex = z
+})
+
 slides.addEventListener('click', (e) => {
-  z++
+  z--
   images[current].style.zIndex = z
   current++
   current = current % images.length
